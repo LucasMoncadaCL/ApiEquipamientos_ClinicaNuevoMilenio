baf3f794196ef1b7ae31ef637171d5329d684574
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-25 01:37:44 -0400

    commit de la api Equipamiento completa. puerto 8004

--- src/main/java/com/clinicanuevomilenio/equipamientos/repository/EquipamientoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clinicanuevomilenio.equipamientos.repository;
-
-import com.clinicanuevomilenio.equipamientos.models.Equipamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipamientoRepository extends JpaRepository<Equipamiento, Integer> {
-}
