9f6bb48be9f5be190e040ad68d8ca6e5137f434b
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-25 00:28:58 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/clinicanuevomilenio/equipamientos/EquipamientosApplication.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.equipamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicanuevomilenio/equipamientos/EquipamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.equipamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
